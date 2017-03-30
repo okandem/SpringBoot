0c29b665515874acc09cfdeb6273730756d5ad0d
Okan Demirtaş <demirtasokan@gmail.com>
2017-03-30 22:01:12 +0300

    test resoursec data.sql eklendi. test-data.sql ile pupulate edildi.

--- src/test/java/org/test/DemoApplicationTests.java
+++ src/test/java/org/test/DemoApplicationTests.java
@@ -8,12 +8,16 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.test.entity.Book;
 import org.test.repository.BookRepository;
+
+import javax.sql.DataSource;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
@@ -36,23 +40,35 @@ public class DemoApplicationTests {
 	private WebApplicationContext context;
 	@Autowired
 	private BookRepository repository;
+	@Autowired
+	private DataSource ds;
 	@LocalServerPort
 	private int port;
 
 
 	private MockMvc mockMvc;
 	private TestRestTemplate restTemplate = new TestRestTemplate();
+	private static boolean loadDataFixtures =true;
 
 @Before
 public void setupMockMvc(){
 	mockMvc= MockMvcBuilders.webAppContextSetup(context).build();
 }
 
+@Before
+public void setLoadDataFixtures(){
+	if(loadDataFixtures){
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator(context.getResource("classpath:/test-data.sql"));
+		DatabasePopulatorUtils.execute(populator,ds);
+		loadDataFixtures=false;
+	}
+}
+
 
 	@Test
 	public void contextLoads() {
 
-		assertEquals(1,repository.count());
+		assertEquals(4,repository.count());
 	}
 
 	@Test
